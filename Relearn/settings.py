bf0d45271b835284c5fd70e4512d474762547675
Paulo Endoh <paulo.endoh@gmail.com>
2019-08-20 09:18:10 -0300

    Added allowed HOSTS

--- Relearn/settings.py
+++ Relearn/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'w*rka^+$8kzytb0x749n0e^r*6ts%i
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['immense-brook-32988.herokuapp.com']
 
 
 # Application definition
